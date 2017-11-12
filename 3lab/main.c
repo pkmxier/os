b2f07c187d7c7365dee9693e4bc9ebb39c83095e
pkmxier <lobov.vladislaw@yandex.ru>
2017-11-12 15:39:55 +0300

    3lab final fix

--- 3lab/main.c
+++ 3lab/main.c
@@ -3,16 +3,45 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+int count = 0;
+pthread_mutex_t mutex;
+
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
 int are_cards_equal(int first, int second) {
     return first % 13 == second || first == second % 13;
 }
 
 const int DECK_SIZE = 52;
 
-int count = 0;
-void *potok(void *arg);
+void * thread_func(void * arg) {
+    int deck[DECK_SIZE];
+    
+    for (int k = 0; k < *((int *)arg); ++k) {
+        for (int i = 0; i < DECK_SIZE; ++i) {
+            deck[i] = i + 1;
+        }
 
-pthread_mutex_t mutex;
+        srand(clock());
+
+        for (int i = DECK_SIZE - 1; i >= 0; --i) {
+            int j = (int)rand() % (i + 1);
+            swap(&deck[i], &deck[j]);
+        }
+
+        if (are_cards_equal(deck[0], deck[1])) {
+            pthread_mutex_lock(&mutex);
+            ++count;
+            pthread_mutex_unlock(&mutex);
+        }
+    }
+    
+    pthread_exit(0);
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -20,19 +49,17 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int rounds_per_thread = atoi(argv[1]) / atoi(argv[2]);
-
     pthread_t tid[atoi(argv[2])];
 
     pthread_mutex_init(&mutex, NULL);
-
+    
+    int rounds_per_thread = atoi(argv[1]) / atoi(argv[2]);
     for (int i = 0; i < atoi(argv[2]); ++i) {
-        if (pthread_create(&tid[i], NULL, potok, &rounds_per_thread) != 0) {
+        if (pthread_create(&tid[i], NULL, thread_func, &rounds_per_thread) != 0) {
             perror("Can`t create thread\n");
             return 2;
         }
     }
-
     for (int i = 0; i < atoi(argv[2]); ++i) {
         if (pthread_join(tid[i], NULL) != 0) {
             perror("Can`t join threads");
@@ -43,29 +70,6 @@ int main(int argc, char *argv[]) {
     pthread_mutex_destroy(&mutex);
 
     printf("%lf", (double)count / atoi(argv[1]));
+    return 0;
 }
 
-void *potok(void * arg) {
-    int deck[DECK_SIZE];
-    for (int k = 0; k < *((int *)arg); ++k) {
-        for (int i = 0; i < DECK_SIZE; ++i) {
-            deck[i] = i + 1;
-        }
-
-        srand(clock());
-
-        for (int i = DECK_SIZE - 1; i >= 0; --i) {
-            int j = (int)rand() % (i + 1);
-            int tmp = deck[i];
-            deck[i] = deck[j];
-            deck[j] = tmp;
-        }
-
-        if (are_cards_equal(deck[0], deck[1])) {
-            pthread_mutex_lock(&mutex);
-            ++count;
-            pthread_mutex_unlock(&mutex);
-        }
-    }
-    pthread_exit(0);
-}
