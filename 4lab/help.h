b4d18fabec78e590897aa67fea6705c9b3639e0d
pkmxier <lobov.vladislaw@yandex.ru>
2017-11-13 14:56:29 +0300

    oh

---
+++ 4lab/help.h
@@ -0,0 +1,87 @@
+#ifndef HELP_H
+#define HELP_H
+
+#define HELP 1
+#define EXIT 2
+#define SWITCH 3
+#define STATUS 4
+#define GETLINE 5
+#define FIND 6
+#define WHERETOSEARCH 7
+#define BEGIN 700
+#define END 800
+#define ETC 900
+#define WRITE 10
+#define ERASE 11
+#define LIMIT 12
+#define FILE 13
+#define PRINT 14
+#define NOTCOMMAND -1
+
+int IsDecimal(char *str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] < '0' || str[i] > '9') {
+            return 0;
+        }
+        ++i;
+    }
+    return 1;
+}
+
+int StrToInt(char *string) {
+    if (strcmp(string, "/help") == 0) {
+        return HELP;
+    } else if (strcmp(string, "/exit") == 0) {
+        return EXIT;
+    } else if (strcmp(string, "/switch") == 0) {
+        return SWITCH;
+    } else if (strcmp(string, "/status") == 0) {
+        return STATUS;
+    } else if (strcmp(string, "/getline") == 0) {
+        return GETLINE;
+    } else if (strcmp(string, "/find") == 0) {
+        return FIND;
+    } else if (strcmp(string, "/begin") == 0) {
+        return WHERETOSEARCH;
+    } else if (strcmp(string, "/end") == 0) {
+        return WHERETOSEARCH;
+    } else if (strcmp(string, "/etc") == 0) {
+        return WHERETOSEARCH;
+    } else if (strcmp(string, "/write") == 0) {
+        return WRITE;
+    } else if (strcmp(string, "/erase") == 0) {
+        return ERASE;
+    } else if (strcmp(string, "/limit") == 0) {
+        return LIMIT;
+    } else if (strcmp(string, "/file") == 0) {
+        return FILE;
+    } else if (strcmp(string, "/print") == 0) {
+        return PRINT;
+    } else {
+        return NOTCOMMAND;
+    }
+}
+
+void PrintUsage() {
+    printf("Usage: ./a.out /\"command name\" /file \"path\"\n"
+           "commands:\n"
+           "    help - print usage\n"
+           "    exit - exit from program\n"
+           "    switch /file:\"filename\" - change current file to the given\n"
+           "    status - print lines' and symbols' count\n"
+           "    getline \"number of line\" - print line\n"
+           "    find begin/end/etc \"pattern\" - find word, which the pattern begins/ends/contains\n"
+           "    write \"position\" \"text\" - write text at the position\n"
+           "    erase \"position\" \"number of symbols\" - delete some symbols\n"
+           "    limit \"min number of symbols\" \"max number of symbols\" - set up min and max size of file\n");
+}
+
+void PrintWrong() {
+    printf("Wrong command\n"
+           "Type \"/help\" for usage\n");
+}
+
+const int DEFAULT_MIN = 0;
+const int DEFAULT_MAX = __INT_MAX__;
+#endif // HELP_H
--- 4lab/main.c
+++ 4lab/main.c
@@ -1,87 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define HELP 1
-#define EXIT 2
-#define SWITCH 3
-#define STATUS 4
-#define GETLINE 5
-#define FIND 6
-#define WHERETOSEARCH 7
-#define BEGIN 700
-#define END 800
-#define ETC 900
-#define WRITE 10
-#define ERASE 11
-#define LIMIT 12
-#define FILE 13
-#define NOTCOMMAND -1
-
-int IsDecimal(char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        if (str[i] < '0' || str[i] > '9') {
-            return 0;
-        }
-        ++i;
-    }
-    return 1;
-}
-
-int StrToInt(char *string) {
-    if (strcmp(string, "/help") == 0) {
-        return HELP;
-    } else if (strcmp(string, "/exit") == 0) {
-        return EXIT;
-    } else if (strcmp(string, "/switch") == 0) {
-        return SWITCH;
-    } else if (strcmp(string, "/status") == 0) {
-        return STATUS;
-    } else if (strcmp(string, "/getline") == 0) {
-        return GETLINE;
-    } else if (strcmp(string, "/find") == 0) {
-        return FIND;
-    } else if (strcmp(string, "/begin") == 0) {
-        return WHERETOSEARCH;
-    } else if (strcmp(string, "/end") == 0) {
-        return WHERETOSEARCH;
-    } else if (strcmp(string, "/etc") == 0) {
-        return WHERETOSEARCH;
-    } else if (strcmp(string, "/write") == 0) {
-        return WRITE;
-    } else if (strcmp(string, "/erase") == 0) {
-        return ERASE;
-    } else if (strcmp(string, "/limit") == 0) {
-        return LIMIT;
-    } else if (strcmp(string, "/file") == 0) {
-        return FILE;
-    } else {
-        return NOTCOMMAND;
-    }
-}
-
-void PrintUsage() {
-    printf("Usage: ./a.out /\"command name\" /file \"path\"\n"
-           "commands:\n"
-           "    help - print usage\n"
-           "    exit - exit from program\n"
-           "    switch /file:\"filename\" - change current file to the given\n"
-           "    status - print lines' and symbols' count\n"
-           "    getline \"number of line\" - print line\n"
-           "    find begin/end/etc \"pattern\" - find word, which the pattern begins/ends/contains\n"
-           "    write \"position\" \"text\" - write text at the position\n"
-           "    erase \"position\" \"number of symbols\" - delete some symbols\n"
-           "    limit \"min number of symbols\" \"max number of symbols\" - set up min and max size of file\n");
-}
-
-void PrintWrong() {
-    printf("Wrong command\n"
-           "type \"help\" for usage\n");
-}
-
-const int DEFAULT_MIN = 0;
-const int DEFAULT_MAX = __INT_MAX__;
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include "help.h"
+#include <errno.h>
 
 typedef struct {
     int fd;
@@ -91,26 +17,198 @@ typedef struct {
 
 TFILE * OpenFile(char *path, int min, int max) {
     TFILE *file = (TFILE *)malloc(sizeof(TFILE));
-//    open(&file->fd, ...);
-//    if (file->fd < 0) {
-
-//    }
+    file->fd = open(path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
     file->min = min;
     file->max = max;
+
+    if (file->fd < 0) {
+        free(file);
+        file = NULL;
+    }
+
     return file;
+}
+
+int IsDigitOrLetter(char c) {
+    return (c >= '0' && c <= '9') ||
+           (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z');
+}
+void PrintStatus(TFILE *file) {
+    int symbolsCount = 0;
+    int linesCount = 0;
+    char c;
+
+    while (read(file->fd, &c, 1) == 1) {
+        if (IsDigitOrLetter(c)) {
+            ++symbolsCount;
+        } else if (c == '\n') {
+            ++linesCount;
+        }
+    }
+
+    if (c != '\n') {
+        ++linesCount;
+    }
+
+    if (linesCount == 0) {
+        ++linesCount;
+    }
+
+    printf("Number of lines: %d, number of symbols: %d\n", linesCount, symbolsCount);
+}
+
+void FindPattern(TFILE *file, char *mode, char *pattern) {
+    char c;
+    int i = 0;
+    int len = strlen(pattern);
+    int size = len > sysconf(_SC_PAGE_SIZE) ? len : sysconf(_SC_PAGESIZE);
+
+    char *tmp = (char *)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, file->fd, SEEK_SET);
+    tmp[size] = '\0';
+
+    if (!strstr(tmp, pattern)) {
+        printf("Can`t find such word\n");
+    } else {
+        printf("Yes\n");
+    }
+}
+
+void PrintLine(TFILE *file, int number) {
+    char c;
+
+    while (number != 1 && read(file->fd, &c, 1) == 1) {
+        if (c == '\n') {
+            --number;
+        }
+    }
+
+    if (c == '\n') {
+        ++number;
+    }
+
+    if (number != 1) {
+        printf("Wrong line\n");
+        return;
+    }
+
+    while (read(file->fd, &c, 1) == 1 && c != '\n') {
+        printf("%c", c);
+    }
+    printf("\n");
+}
+
+void WriteWithin(TFILE *file, int position, char *text) {
+    --position;
+    lseek(file->fd, position, SEEK_SET);
+    int fd = open("./", __O_TMPFILE | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd < 0) {
+        printf("Can`t create temporary file\n");
+        return;
+    }
+
+    char c;
+    while (read(file->fd, &c, 1) == 1) {
+        if (write(fd, &c, 1) != 1) {
+            printf("Can`t write in temporary file\n");
+            return;
+        }
+    }
+
+    if (ftruncate(file->fd, position) < 0) {
+        printf("Can`t erase from that position\n");
+        return;
+    }
+
+    lseek(file->fd, position, SEEK_SET);
+    if (write(file->fd, text, strlen(text)) != strlen(text)) {
+        printf("Can`t write in this file\n");
+        lseek(file->fd, position, SEEK_SET);
+    }
+
+    lseek(fd, 0, SEEK_SET);
+    while (read(fd, &c, 1) == 1) {
+        if (write(file->fd, &c, 1) != 1) {
+            printf("Can`t write in this file\n");
+            return;
+        }
+    }
+}
+
+void EraseWithin(TFILE *file, int position, int count) {
+    --position;
+    lseek(file->fd, position + count, SEEK_SET);
+    int fd = open("./", __O_TMPFILE | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd < 0) {
+        printf("Can`t create temporary file\n");
+        return;
+    }
+
+    char c;
+    while (read(file->fd, &c, 1) == 1) {
+        if (write(fd, &c, 1) != 1) {
+            printf("Can`t write in temporary file\n");
+            return;
+        }
+    }
+
+    if (ftruncate(file->fd, position) < 0) {
+        printf("Can`t erase from that position\n");
+        return;
+    }
+
+    lseek(file->fd, position, SEEK_SET);
+    lseek(fd, 0, SEEK_SET);
+    while (read(fd, &c, 1) == 1) {
+        if (write(file->fd, &c, 1) != 1) {
+            printf("Can`t write in this file\n");
+            return;
+        }
+    }
+}
+
+void SetLimit(TFILE *file, int min, int max) {
+    int i = 0;
+    int c;
+    while (read(file->fd, &c, 1) == 1) {
+        ++i;
+
+        if (i + 1 > max) {
+            break;
+        }
+    }
+
+    if (i + 1 > max) {
+        if (ftruncate(file->fd, max) < 0) {
+            printf("Can`t trunc file\n");
+            return;
+        }
+    } else if (i < min) {
+        while (i < min) {
+            if (write(file->fd, "0", 1) != 1) {
+                return;
+            }
+            ++i;
+        }
+    }
+}
+
+void PrintFile(TFILE *file) {
+    char c;
+    while (read(file->fd, &c, 1) == 1) {
+        printf("%c", c);
+    }
 }
 
 int main(int argc, char *argv[]) {
     TFILE *cur;
 
-    if (argc == 1) {
-        printf("Interactive mode:\n");
-    } else if (argc == 2) {
+    if (argc == 2) {
         if (StrToInt(argv[1]) == HELP) {
             PrintUsage();
         } else {
-            printf("Wrong command\n"
-                   "type \"help\" for usage\n");
+            PrintWrong();
+            return 1;
         }
     } else if (argc >= 3 && argc <= 6) {
         switch(StrToInt(argv[1])) {
@@ -133,6 +231,9 @@ int main(int argc, char *argv[]) {
                         printf("Can`t open file\n");
                         return 2;
                     }
+
+                    PrintStatus(cur);
+                    return 0;
                 } else {
                     PrintWrong();
                     return 1;
@@ -145,6 +246,9 @@ int main(int argc, char *argv[]) {
                         printf("Can`t open file\n");
                         return 2;
                     }
+
+                    PrintLine(cur, atoi(argv[2]));
+                    return 0;
                 } else {
                     PrintWrong();
                     return 1;
@@ -157,6 +261,9 @@ int main(int argc, char *argv[]) {
                         printf("Can`t open file\n");
                         return 2;
                     }
+
+                    FindPattern(cur, argv[2], argv[3]);
+                    return 0;
                 } else {
                     PrintWrong();
                     return 1;
@@ -169,6 +276,9 @@ int main(int argc, char *argv[]) {
                         printf("Can`t open file\n");
                         return 2;
                     }
+
+                    WriteWithin(cur, atoi(argv[2]), argv[3]);
+                    return 0;
                 } else {
                     PrintWrong();
                     return 1;
@@ -181,6 +291,9 @@ int main(int argc, char *argv[]) {
                         printf("Can`t open file\n");
                         return 2;
                     }
+
+                    EraseWithin(cur, atoi(argv[2]), atoi(argv[3]));
+                    return 0;
                 } else {
                     PrintWrong();
                     return 1;
@@ -193,6 +306,9 @@ int main(int argc, char *argv[]) {
                         printf("Can`t open file\n");
                         return 2;
                     }
+
+                    SetLimit(cur, atoi(argv[2]), atoi(argv[3]));
+                    return 0;
                 } else {
                     PrintWrong();
                     return 1;
@@ -210,11 +326,27 @@ int main(int argc, char *argv[]) {
                     return 1;
                 }
                 break;
+            case PRINT:
+                if (argc == 4 && StrToInt(argv[2]) == FILE) {
+                    cur = OpenFile(argv[3], DEFAULT_MIN, DEFAULT_MAX);
+                    if (cur == NULL) {
+                        printf("Can`t open file\n");
+                        return 2;
+                    }
+
+                    PrintFile(cur);
+                } else {
+                    PrintWrong();
+                    return 1;
+                }
+                break;
             default:
                 PrintWrong();
                 return 1;
                 break;
         }
+    } else if (argc != 1) {
+        PrintWrong();
     }
     return 0;
 }
