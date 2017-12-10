e6dcf2f2e618f6634689c8cd20f535bfbd420e35
pkmxier <lobov.vladislaw@yandex.ru>
2017-12-10 22:16:29 +0300

    6lab not done

---
+++ 6lab/6lab.pro
@@ -0,0 +1,12 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += main.cpp \
+    bank.cpp \
+    server.cpp \
+    client.cpp
+
+HEADERS += \
+    bank.h
---
+++ 6lab/server.cpp
@@ -0,0 +1,82 @@
+#include "bank.h"
+#include <iostream>
+#include <cstring>
+#include "zmq.h"
+#include <unistd.h>
+#include <string>
+
+typedef struct MD
+{
+    int clientId;
+    int messageNumber;
+    char message[128];
+    int action;
+    int amount;
+} MessageData;
+
+int main(int argc, char const *argv[])
+{
+    void* context = zmq_ctx_new();
+    void* serverSocket = zmq_socket(context, ZMQ_REP);
+    char address[32];
+    int bank_id;
+    std::cin >> bank_id;
+    sprintf(address, "%s%d", "tcp://*:", bank_id);
+    zmq_bind(serverSocket, address);
+    std::cout << "Starting..." << std::endl;
+    Bank b;
+    for (;;)
+    {
+        zmq_msg_t message;
+        zmq_msg_init(&message);
+        zmq_msg_recv(&message, serverSocket, 0);
+        MessageData *m = (MessageData *)zmq_msg_data(&message);
+        printf("Message from client: %d  messageId: %d message: ", m->clientId, m->messageNumber);
+        std::cout <<  m->message << std::endl;
+        zmq_msg_close(&message);
+
+        b.add_new_client(m->clientId);
+
+        zmq_msg_t reply;
+        char answer[128];
+
+        switch(m->action) {
+            case 1:
+                if (b.get_money(m->clientId, m->amount)) {
+                    sprintf(answer, "Here is your %d dollars", m->amount);
+                    zmq_msg_init_size(&reply, 128);
+                    memcpy(zmq_msg_data(&reply), answer, 128);
+                    std::cout << "I`ve just given " << m->amount << " dollars to " << m->clientId << "..." << std::endl;
+                } else {
+                    sprintf(answer, "You don`t have enough money");
+                    zmq_msg_init_size(&reply, strlen(answer) + 1);
+                    memcpy(zmq_msg_data(&reply), answer, strlen(answer) + 1);
+                    std::cout << "Haha, " << m->clientId << " doesn`t have " << m->amount << " dollars!" << std::endl;
+                }
+                break;
+            case 2:
+                b.add_money(m->clientId, m->amount);
+                sprintf(answer, "Thank you for %d dollars", m->amount);
+                zmq_msg_init_size(&reply, strlen(answer) + 1);
+                memcpy(zmq_msg_data(&reply), answer, strlen(answer) + 1);
+                std::cout << m->clientId << " has just put " << m->amount << " dollars into me!" << std::endl;
+                break;
+            case 3:
+                sprintf(answer, "Your balance is %d", b.balance(m->clientId));
+                zmq_msg_init_size(&reply, strlen(answer) + 1);
+                memcpy(zmq_msg_data(&reply), answer, strlen(answer) + 1);
+                std::cout << m->clientId << " has just requested his balance" << std::endl;
+                break;
+            case 4:
+
+                break;
+        }
+
+        zmq_msg_send(&reply, serverSocket, 0);
+        zmq_msg_close(&reply);
+    }
+    zmq_close(serverSocket);
+    zmq_ctx_destroy(context);
+
+    return 0;
+}
